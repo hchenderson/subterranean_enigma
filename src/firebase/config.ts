56ad03dfe0e8db3f3bdb9042449f52234a39f446
Carter Henderson <hendersonranch3@gmail.com>
2025-12-08 01:04:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7172235120-1ba28",
+  "appId": "1:329723142208:web:09361955e5a7d7c4c1bfa5",
+  "apiKey": "AIzaSyBzcuQkQWfGj3OJ-w95-CL7iqYW45fWSBM",
+  "authDomain": "studio-7172235120-1ba28.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "329723142208"
+};
